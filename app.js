196cdfa99b77cf779c9157eb3b68ce49a4401377
nguyenquocduycm <nguyenquocduycm@gmail.com>
2021-04-08 16:55:16 +0700

    add cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const mongoose= require("mongoose");
 const morgan = require("morgan");
-
+const cors=require("cors");
 const accountRoutes=require("./API/routes/account");
 
 
@@ -20,7 +20,7 @@ app.use(morgan('dev'));
 //app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 //app.use(bodyParser.json());
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
