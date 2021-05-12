b19528d47289aa87996697ddead1dd97380edb73
nguyenquocduycm <nguyenquocduycm@gmail.com>
2021-05-12 13:41:58 +0700

    updte

--- API/routes/coursesContent.js
+++ API/routes/coursesContent.js
@@ -5,8 +5,8 @@ const coursesContentController =require("../controllers/coursesContent");
 const check_auth =require("../middleware/check-auth");
 
 
-router.get("/",check_auth,coursesContentController.Get_One_Courses);
+router.post("/",check_auth,coursesContentController.Get_One_Courses);
 
-router.get("/check",check_auth,coursesContentController.Check_Change_Courses);
+router.post("/check",check_auth,coursesContentController.Check_Change_Courses);
 
 module.exports=router;
--- API/routes/studyCourses.js
+++ API/routes/studyCourses.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const studycouresController=require("../controllers/studyCourses");
 const check_auth=require("../middleware/check-auth");
 
-router.get("/allcourses",check_auth,studycouresController.Get_ListCoures);
+router.post("/allcourses",check_auth,studycouresController.Get_ListCoures);
 
 router.get("/currentcourses",check_auth,studycouresController.Get_CurrentCourses);
 
