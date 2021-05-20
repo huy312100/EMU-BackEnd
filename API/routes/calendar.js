1e3d7eb5d757615fc2f731c587fbf9347545b514
nguyenquocduycm <nguyenquocduycm@gmail.com>
2021-05-20 11:25:14 +0700

    editpost

--- API/routes/calendar.js
+++ API/routes/calendar.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const CalendarController = require("../controllers/calendar");
 const check_auth =require("../middleware/check-auth");
 
-router.get("/getthismonth",check_auth,CalendarController.Get_Calendar_This_Month);
+router.post("/getthismonth",check_auth,CalendarController.Get_Calendar_This_Month);
 
 router.post("/post",check_auth,CalendarController.Post_Calendar);
 
