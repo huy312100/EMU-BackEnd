c5bdc2314cbd9a9002bc0bf3eecddff6b94b7540
nguyenquocduycm <nguyenquocduycm@gmail.com>
2021-05-27 06:55:21 +0700

    change socket

--- API/middleware/socket.js
+++ API/middleware/socket.js
@@ -2,6 +2,7 @@ const socket_io = require('socket.io');
 const Account = require("../models/account");
 const chat = require("../models/chat");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 // const io = socket_io();
 
 // var Socket = {
@@ -24,9 +25,9 @@ const jwt = require("jsonwebtoken");
 
 exports.OnSocket =(socket)=>{
     var FromUser
-    socket.on("Create-Room",(user1)=>{
+    socket.on("Create-Room",(user)=>{
         try {
-            const decoded= jwt.verify(user1[0],process.env.JWT_KEY);
+            const decoded= jwt.verify(user[0],process.env.JWT_KEY);
             FromUser=decoded.username;
             if(FromUser.length>=1){
                 Account.find({username:FromUser})
@@ -34,26 +35,35 @@ exports.OnSocket =(socket)=>{
                 .then(re1=>{
                     if(re1.length>=1){
                         //socket.emit("Reply-Create-Room","created")
-                        Account.find({username:user1[1]})
+                        Account.find({username:user[1]})
                         .exec()
                         .then(re2=>{
                             if(re2.length>=1){
+                                chat.find({User:{$in:[user[0],user[1]]}})
+                                .exec()
+                                .then(re3=>{
+                                    if(re3.length>=1){
+                                        var Chat = new chat({
+                                            _id: new mongoose.Types.ObjectId(),
+                                            User:re1[0]._id,
+                                            TypeRoom:"TwoPeple",
+                                            chat:[]
+                                        })
+                                        User.push(re2[0]._id);
+                                        var Idroom= Chat._id;
+                                        Chat.save()
+                                        .then(()=>{
+                                            socket.emit("Reply-Create-Room",Idroom.toString());
+                                        })
+                                        .catch(err=>{
+                                            socket.emit("Reply-Create-Room","error");
+                                        })
+                                    }
+                                    else{
+                                        socket.emit("Reply-Create-Room",re3[0]._id);
+                                    }
+                                })
                                 
-                                var Chat = new chat({
-                                    _id: new mongoose.Types.ObjectId(),
-                                    User1:re1._id,
-                                    User2:re2._id,
-                                    TypeRoom:"TwoPeple",
-                                    chat:[]
-                                })
-                                var Idroom= Chat._id;
-                                Chat.save()
-                                .then(()=>{
-                                    socket.emit("Reply-Create-Room",Idroom.toString());
-                                })
-                                .catch(err=>{
-
-                                })
                                 
                             }else{
                                 socket.emit("Reply-Create-Room","error");
--- API/models/chat.js
+++ API/models/chat.js
@@ -8,8 +8,8 @@ const ChatContext = mongoose.Schema({
 
 const chatSchema = mongoose.Schema({
     __id: mongoose.Schema.Types.ObjectId,
-    User1:  {type:mongoose.Schema.Types.ObjectId, ref:'Account'},
-    User2:  {type:mongoose.Schema.Types.ObjectId, ref:'Account'},
+    User:  {type:[mongoose.Schema.Types.ObjectId], ref:'Account'},
+    //User2:  {type:mongoose.Schema.Types.ObjectId, ref:'Account'},
     TypeRoom: {type:String, require:true},
     chat:{type:[ChatContext], default:[]}
 });
