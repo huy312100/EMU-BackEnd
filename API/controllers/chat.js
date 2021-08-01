82a7381eef55b712b0c88c4fd5c48188021d9747
nguyenquocduycm <nguyenquocduycm@gmail.com>
2021-08-02 00:37:58 +0700

    edit chat

--- API/controllers/chat.js
+++ API/controllers/chat.js
@@ -11,18 +11,39 @@ exports.FindChatAwait = (req, res, next) => {
         .exec()
         .then(async (re1) => {
             if (re1.length >= 1) {
+                var results = [];
                 for (var i = 0; i < re1[0].awaittext.length; i++) {
                     try {
                         let pool = await sql.connect(Config);
 
                         let profiles = await pool.request()
                             .input('ID_Signin', sql.VarChar, re1[0].awaittext[i].from)
-                            .query("SELECT [HoTen] FROM [dbo].[InfoSinhVien] where InfoSinhVien.Email = @ID_Signin");
+                            .query("SELECT [HoTen], [AnhSV] FROM [dbo].[InfoSinhVien] where InfoSinhVien.Email = @ID_Signin");
 
                         //console.log(facultys.recordsets[0]);
                         if (profiles.recordsets[0]) {
-                            re1[0].awaittext[i].from = profiles.recordsets[0][0]["HoTen"];
-                            console.log(profiles.recordsets[0][0]);
+                            // re1[0].awaittext[i].from = profiles.recordsets[0][0]["HoTen"];
+                            // console.log(profiles.recordsets[0][0]);
+
+
+                            //var leng = re1[i].chat.length;
+                            var temp = {
+                                "name": profiles.recordsets[0][0]["HoTen"],
+                                "Email": re1[0].awaittext[i].from,
+                                "Anh": profiles.recordsets[0][0]["AnhSV"],
+                                "TypeRoom": "TwoPeople",
+                                "text": re1[0].awaittext[i].text,
+                                "time": parseInt(re1[0].awaittext[i].time),
+                                "state": re1[0].awaittext[i].state,
+                            }
+                            if (results !== undefined) {
+                                results.push(temp);
+                            }
+                            else {
+                                results = temp;
+                            }
+                            //console.log(results);
+
                         }
                         else {
                             res.status(500).json({ err: "err" });
@@ -36,7 +57,7 @@ exports.FindChatAwait = (req, res, next) => {
                     //res.status(200).json(re1[0]);
                 }
 
-                res.status(200).json(re1[0]);
+                res.status(200).json(results);
             }
             else {
                 res.status(200).json({ message: "Message await is Empty" });
